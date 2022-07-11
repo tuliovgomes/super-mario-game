8040b9c86e17381c948d299102eeb430b4e44b87
Rochele Edenís <rochele.edenis@gmail.com>
2022-07-10 22:49:37 -0300

    Allows the player to restart the game

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const gameOver = document.querySelector('.game-over');
 
 const jump = () => {
     mario.classList.add('jump');
+    console.log('pule');
 
     setTimeout(() => {
         mario.classList.remove('jump');
     }, 500);
 }
 
-const loop = setInterval(() => {
+const waitingFailure = () => {
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
@@ -18,19 +20,39 @@ const loop = setInterval(() => {
         && pipePosition > 0
         && marioPosition < 112
     ) {
+        mario.style.animationPlayState = 'paused';
+        mario.style.bottom = `${marioPosition}px`;
+
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
-
-        mario.style.animation = 'none';
-        mario.style.bottom = `${marioPosition}px`;
 
         mario.src = './images/game-over.png';
         mario.style.width = '75px';
         mario.style.marginLeft = '50px';
 
+        gameOver.style.display = 'block';
+
         clearInterval(loop);
     }
-}, 10);
+};
 
+var loop = setInterval(waitingFailure, 10);
+
+const restartGame = function () {
+    gameOver.style.display = 'none';
+
+    mario.style.animationPlayState = 'running';
+    mario.src = './images/mario.gif';
+    mario.style.width = '150px';
+    mario.style.marginLeft = '0';
+    mario.style.bottom = '0';
+
+    pipe.style.left = 'auto';
+    pipe.style.animation = 'pipe-animation 1s infinite linear';
+
+    loop = setInterval(waitingFailure, 10);
+};
+
+document.querySelector('.retry').addEventListener('click', restartGame);
 document.addEventListener('keydown', jump);
 document.addEventListener('touchstart', jump);
