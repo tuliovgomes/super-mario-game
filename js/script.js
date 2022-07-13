2881bc455c6bb9c741d803f0453a7408370884ce
Rochele Edenís <rochele.edenis@gmail.com>
2022-07-12 22:40:33 -0300

    Adding score

--- js/script.js
+++ js/script.js
@@ -1,10 +1,54 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const gameOver = document.querySelector('.game-over');
+const score = document.querySelector('.score');
+const highScore = document.querySelector('#high-score');
+
+const setCookie = function (name, value, expirationDays) {
+    const date = new Date();
+    date.setTime(date.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + date.toUTCString();
+
+    document.cookie = name + "=" + value + ";" + expires + ";SameSite=Lax;path=/";
+};
+
+const getCookie = function (name) {
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    name = name + "=";
+
+    console.log(ca);
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+
+    return "";
+};
+
+var scoreValue = -1;
+var highScoreValue = getCookie('high-score');
+
+highScore.textContent = getCookie('high-score');
 
 const jump = () => {
     mario.classList.add('jump');
-    console.log('pule');
+
+    scoreValue += 1;
+    score.textContent = scoreValue;
+
+    if (highScoreValue < scoreValue) {
+        setCookie('high-score', scoreValue, 365);
+        highScoreValue = scoreValue;
+        highScore.textContent = highScoreValue;
+    }
 
     setTimeout(() => {
         mario.classList.remove('jump');
@@ -53,14 +97,16 @@ const restartGame = function () {
     pipe.style.left = 'auto';
     pipe.style.animation = 'pipe-animation 1s infinite linear';
 
+    scoreValue = 0;
+    score.textContent = scoreValue;
+
     document.addEventListener('keydown', jump);
     document.addEventListener('touchstart', jump);
 
     loop = setInterval(waitingFailure, 10);
 };
 
-document.querySelector('.retry')
-    .addEventListener('click', restartGame);
+document.querySelector('.retry').addEventListener('click', restartGame);
 
 document.querySelector('.game-board').addEventListener('keydown', jump);
 document.querySelector('.game-board').addEventListener('touchstart', jump);
