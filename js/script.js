eae98ab515cd89a83eb969d5df60397e838d0c9a
Túlio Gomes <tuliogomes07@hotmail.com>
2022-07-28 22:30:43 -0300

    fix game start

--- js/script.js
+++ js/script.js
@@ -4,13 +4,10 @@ const turtle = document.querySelector('.turtle');
 const bullet = document.querySelector('.bullet');
 const clouds = document.querySelector('.clouds');
 const gameOver = document.querySelector('.game-over');
+const gameStarter = document.querySelector('.game-starter');
 const score = document.querySelector('.score');
 const gameBoard = document.getElementById('game-board');
 const highScore = document.querySelector('#high-score');
-
-var musicGame = new Audio('/sound/game.mp3');
-    musicGame.play();
-    musicGame.loop =true;
 
 const setCookie = function (name, value, expirationDays) {
     const date = new Date();
@@ -88,11 +85,11 @@ const jump = () => {
 }
 
 const waitingFailure = () => {
-    const pipePosition = pipe.offsetLeft;
-    const turtlePosition = turtle.offsetLeft;
-    const bulletPosition = bullet.offsetLeft;
-    const cloudsPosition = clouds.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    var pipePosition = pipe.offsetLeft;
+    var turtlePosition = turtle.offsetLeft;
+    var bulletPosition = bullet.offsetLeft;
+    var cloudsPosition = clouds.offsetLeft;
+    var marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
     if (
         (pipePosition <= 100 || turtlePosition <= 100 || bulletPosition <= 100)
@@ -100,23 +97,13 @@ const waitingFailure = () => {
         && marioPosition < 112
     ) {
         musicGame.pause();
-        musicGame = new Audio('sound/mariodie.wav');
-        musicGame.play();
 
-        mario.style.animationPlayState = 'paused';
-        mario.style.bottom = `${marioPosition}px`;
+        if(loop != undefined) {
+            musicDie = new Audio('sound/mariodie.wav');
+            musicDie = musicDie.play();
+        }
 
-        clouds.style.animation = 'none';
-        clouds.style.left = `${cloudsPosition}px`;
-
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`;
-
-        turtle.style.animation = 'none';
-        turtle.style.left = `${turtlePosition}px`;
-
-        bullet.style.animation = 'none';
-        bullet.style.left = `${bulletPosition}px`;
+        stopPosition(pipePosition, turtlePosition, bulletPosition, cloudsPosition, marioPosition);
 
         mario.src = './images/game-over.png';
         mario.style.width = '75px';
@@ -124,13 +111,46 @@ const waitingFailure = () => {
 
         gameOver.style.display = 'block';
 
-        clearInterval(loop);
+        loop = clearInterval(loop);
 
         document.removeEventListener('keydown', jump);
         document.removeEventListener('touchstart', jump);
         gameBoard.classList.add('game-board-over');
     }
 };
+
+stopPosition = (
+    pipePosition,
+    turtlePosition,
+    bulletPosition,
+    cloudsPosition,
+    marioPosition,
+    inital = false
+) => {
+
+    mario.style.animationPlayState = 'paused';
+    clouds.style.animation = 'none';
+    pipe.style.animation = 'none';
+    turtle.style.animation = 'none';
+    bullet.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
+    clouds.style.left = `${cloudsPosition}px`;
+    pipe.style.left = `${pipePosition}px`;
+    turtle.style.left = `${turtlePosition}px`;
+    bullet.style.left = `${bulletPosition}px`;
+
+    if (inital) {
+        gameStarter.style.display = 'block';
+    }
+}
+
+stopPosition(pipe.offsetLeft,
+    turtle.offsetLeft,
+    bullet.offsetLeft,
+    clouds.offsetLeft,
+    +window.getComputedStyle(mario).bottom.replace('px', ''),
+    true
+);
 
 levelPass = () => {
  switch (scoreValue) {
@@ -157,6 +177,7 @@ var loop = setInterval(waitingFailure, 10);
 
 const restartGame = function () {
     gameOver.style.display = 'none';
+    gameStarter.style.display = 'none';
 
     gameBoard.classList.remove('game-board-over');
 
